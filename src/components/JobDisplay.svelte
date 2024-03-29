e4dc5f07a1d9da81c04d2f77c27bc011eafae105
minimo-io <nicolas@minimo.io>
2024-03-29 16:04:43 -0300

    Starting up test

---
+++ src/components/JobDisplay.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import type { JobItem } from "../models";
+    export let job: JobItem;
+</script>
+
+<div class="job-item">
+    <p>Job Title: <strong>{job.job_title}</strong></p>
+    <p>Description: <strong>{job.job_description}</strong></p>
+    <div class="job-details">
+        <span>Company Name: <strong>{job.company_name}</strong></span>
+        <span>Compensation /yr: <strong>{job.compensation_per_year}</strong></span>
+    </div>
+</div>
+
+<style>
+    .job-item {
+        border: 1px solid grey;
+        padding: 2rem;
+        width: 50%;
+        margin: 1rem;
+        border-radius: 10px;
+    }
+
+    .job-details {
+        display: flex;
+        justify-content: space-between;
+    }
+</style>
