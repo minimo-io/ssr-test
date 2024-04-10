6be270bb30f70cc2bd84f28cde8342525df494ab
minimo-io <nicolas@minimo.io>
2024-04-10 18:50:17 -0300

    Ticker positioning!

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,11 @@
+<iframe
+    title="Trading View Ticker"
+    src="https://www.tradingview-widget.com/embed-widget/ticker-tape/"
+    frameborder="0"
+    height="52px"
+    width="100%"
+></iframe>
+
 <nav style="text-align:center;margin-top:1rem;">
     <a href="/">home</a>
     <a href="/about">about</a>
