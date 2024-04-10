0eeb42a32742e9e6e03fd0355930f855d22cb9e5
minimo-io <nicolas@minimo.io>
2024-04-10 12:31:56 -0300

    New tests on ssr vs ssg

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <nav style="text-align:center;margin-top:1rem;">
     <a href="/">home</a>
     <a href="/about">about</a>
+    <a href="/about-2">about 2</a>
     <a href="/noticias">noticias</a>
 </nav>
 
---
+++ src/routes/about-2/+page.svelte
@@ -0,0 +1,10 @@
+<h2>This is our 2nd About page</h2>
+<p>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus vel fuga dolorem modi doloremque ipsam unde
+    quidem quibusdam vitae deleniti, nobis eius molestiae hic. Reiciendis, corrupti. Officiis blanditiis, distinctio
+    mollitia omnis cumque repellat voluptates. Assumenda laboriosam, omnis a quaerat ad excepturi consequatur nostrum
+    eaque nobis obcaecati labore dolorem magnam cupiditate rem eum sapiente amet temporibus suscipit nam porro eveniet
+    iure. Nam reiciendis, natus sunt impedit illo maiores amet quam repellendus tempora voluptatibus earum aspernatur!
+    Aspernatur, explicabo mollitia accusamus eum assumenda provident facilis obcaecati nisi unde nostrum. Sequi facere
+    corrupti ipsam dolores perspiciatis eos, dolore, a ut earum quos doloremque explicabo?
+</p>
